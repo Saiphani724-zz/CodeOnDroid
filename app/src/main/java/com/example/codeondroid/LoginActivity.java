aadbfcb1d1f2878af9919e4849c4f0b144d5a5e1
Prashanth S <prashanthshiv954@gmail.com>
2020-03-23 14:11:16 +0530

    Added Splash Screen among other UI updates

--- app/src/main/java/com/example/codeondroid/LoginActivity.java
+++ app/src/main/java/com/example/codeondroid/LoginActivity.java
@@ -78,7 +78,6 @@ public class LoginActivity extends AppCompatActivity {
         {
             Toast.makeText(LoginActivity.this,"Enter a valid username and password",Toast.LENGTH_SHORT).show();
         }
-
     }
 }
 
